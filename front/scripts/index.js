55c2fbfa4e2cb3e9cff04dbb5c5a89c64c53641a
Diego Carreon <diego24carreon@gmail.com>
2024-04-10 08:47:40 -0600

    Proyecto Modulo 01 Avance | API

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,6 +1,8 @@
+$.get('https://students-api.up.railway.app/movies', (data) => data.map(renderFilms)).fail(() => alert('Error al traer las peliculas'))
+
 const filmsSection = document.getElementById('films')
 
-tempData.map((movie)=> {
+const renderFilms = (movie) => {
     const movieElement = document.createElement('article')
     const containerMovie = document.createElement('div')
 
@@ -10,17 +12,16 @@ tempData.map((movie)=> {
     movieElement.innerHTML = `<img src="${movie.poster}" alt="${movie.title}">`
 
     containerMovie.innerHTML = `
-    <h3>${movie.title}</h3>
-    <p><strong>Año:</strong>${movie.year}</p>
-    <p><strong>Director:</strong>${movie.director}</p>
-    <p><strong>Duracion:</strong>${movie.duration}</p>
-    <p><strong>Genero:</strong>${movie.genre}</p>
-    <p><strong>Rating:</strong>${movie.rate}</p>
+    <h3 id="movieTitleText">${movie.title}</h3>
+    <p><strong>Año:</strong><br>${movie.year}</p>
+    <p><strong>Director:</strong><br>${movie.director}</p>
+    <p><strong>Duracion:</strong><br>${movie.duration}</p>
+    <p><strong>Genero:</strong><br>${movie.genre}</p>
+    <p><strong>Rating:</strong><br>${movie.rate}</p>
     `
 
     movieElement.appendChild(containerMovie)
     filmsSection.appendChild(movieElement)
 
-})
+}
 
-tempData.map(renderFilms)
