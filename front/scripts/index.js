510cd7a234a8cbb4ace7db94ae3629885c98faad
Diego Carreon <diego24carreon@gmail.com>
2024-04-12 09:28:33 -0600

    Proyecto M2 | Webpack

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,28 +1,6 @@
+const renderFilms = require('./renderFilms')
+
 $.get('https://students-api.up.railway.app/movies', (data) => data.map(renderFilms)).fail(() => alert('Error al traer las peliculas'))
 
-const filmsSection = document.getElementById('films')
 
-const renderFilms = (movie) => {
-    const movieElement = document.createElement('article')
-    const containerMovie = document.createElement('div')
-
-    movieElement.classList.add('movie')
-    containerMovie.classList.add('divMovie')
-
-    movieElement.innerHTML = `<img src="${movie.poster}" alt="${movie.title}">`
-
-    containerMovie.innerHTML = `
-    <h3 id="movieTitleText">${movie.title}</h3>
-    <hr>
-    <p><strong>Año:</strong><br>${movie.year}</p>
-    <p><strong>Director:</strong><br>${movie.director}</p>
-    <p><strong>Duracion:</strong><br>${movie.duration}</p>
-    <p><strong>Genero:</strong><br>${movie.genre}</p>
-    <p><strong>Rating:</strong><br>${movie.rate}</p>
-    `
-
-    movieElement.appendChild(containerMovie)
-    filmsSection.appendChild(movieElement)
-
-}
 
