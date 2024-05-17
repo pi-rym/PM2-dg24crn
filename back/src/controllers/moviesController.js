ebc97b28f59c7da5fdf3e5308cb85d5aa2d9ca4e
Diego Carreon <diego24carreon@gmail.com>
2024-05-17 10:09:27 -0600

    Proyecto M2 | Base de Datos

--- back/src/controllers/moviesController.js
+++ back/src/controllers/moviesController.js
@@ -1,8 +1,11 @@
 const { getMoviesService } = require('../services/moviesService')
 
 const moviesController = (req, res) => {
+
     const movies = getMoviesService()
+
     res.status(200).send(movies)
+    
 }
 
 module.exports = moviesController
